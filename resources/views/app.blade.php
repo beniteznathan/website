62e200c56e76588de9468a6f789145acea6ecfde
Jonathan Benitez <beniteznathan.dev@gmail.com>
2022-09-06 11:23:06 +0800

    Added linked assests on app.css file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,16 +9,12 @@
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;1,600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         @vite([
             'resources/js/app.js',
             'resources/css/app.css'
         ])
     </head>
-    <body class="antialiased" id="app">
 
-
-    </body>
+    <body class="antialiased" id="app"></body>
 </html>
